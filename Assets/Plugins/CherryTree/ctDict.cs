c8e58a535e12d6544f753ecae8b951a068b57919
Yonathan Randyanto <yonathan.randyanto@gmail.com>
2016-02-02 15:40:02 +0800

    add ctDict features

---
+++ Assets/Plugins/CherryTree/ctDict.cs
@@ -0,0 +1,29 @@
+﻿
+// https://github.com/Randyanto/CherryTree
+//------------------------------------------
+// 2016/02/02  Initial version
+//------------------------------------------
+
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class ctDict<T> where T:Object {
+	
+	// USAGE: objDict = ctDict<objType>.CreateDict(arr); 
+	public static Dictionary<string, T> CreateDict(T[] objs){
+		// if array is null
+		if (objs == null) return null;
+		
+		// if array length is smaller or equal to zero 
+		int length = objs.Length;
+		if (length <= 0) return null;			
+		
+		// if array is NOT null and its length is larger than zero
+		Dictionary<string, T> dict = new Dictionary<string, T>();
+		for (int i=0; i<length; i++){
+			dict.Add(objs[i].name, objs[i]);			
+		}
+		return dict;				
+	}		
+		
+}
---
+++ Assets/Scripts/ExampleDict.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ExampleDict : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	private Dictionary<string, GameObject> objDict;
+	
+	void Awake(){
+		// create objects dictionary
+		objDict = ctDict<GameObject>.CreateDict(prefabs);
+		// log every loaded sounds
+		foreach (string key in objDict.Keys){
+			Debug.Log(key + " is loaded");
+		}
+	}
+	
+}
